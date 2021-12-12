6c5db3ad6b6a71bbe5ee04adf36e448728ab0763
willjr08-pixel <wgraham11@hotmail.co.uk>
2021-12-12 04:00:33 -0500

    Front page HTML now generated from a template

--- lists/tests.py
+++ lists/tests.py
@@ -1,16 +1,17 @@
+from django.http.request import HttpRequest
+from django.template.loader import render_to_string
 from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
-
 from lists.views import home_page
 
 # Create your tests here.
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
+    def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
 
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
@@ -20,6 +21,4 @@ class HomePageTest(TestCase):
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.strip().endswith('</html>'))
 
-        self.assertTemplateUsed(response, 'home.html') 
-
-    
+        self.assertTemplateUsed(response, 'home.html')

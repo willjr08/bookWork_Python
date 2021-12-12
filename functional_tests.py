df2cf59cc69400bc3f2054dac3c160586f56af23
willjr08-pixel <wgraham11@hotmail.co.uk>
2021-12-12 03:15:13 -0500

    End of Chapter 2 Commit

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,27 @@
-# /e/StudentFiles/Quarter05/USD-T/Python/BookWork
+## /e/StudentFiles/Quarter05/USD-T/Python/BookWork
 
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):  
+    def setUp(self):  
+        self.browser = webdriver.Firefox()
+    def tearDown(self):  
+        self.browser.quit()
+    def test_can_start_a_list_and_retrieve_it_later(self):  
+        ## First Connection from User
+        self.browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+        ## User sees 'To-Do' mentioned in Title & Header
+        self.assertIn('To-Do', self.browser.title)  
+        self.fail('Finish the test!') 
+
+        ## User invited to make a to-do item, adds "Buy peacock feathers"
+        ## When enter is pressed, the page updates and displays the item as:
+        #    # "1: Buy peacock feathers"
+        ## There is still a text box to add more items. She adds a new item
+        #    # "2: Use peacock feathers to make a fly"
+        ## Page remembers previous to-do list
+
+if __name__ == '__main__':  
+    unittest.main(warnings='ignore') 
